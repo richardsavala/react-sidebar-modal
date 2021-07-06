75ce03fac8cdaa0e5fddc53f9fe8a1588b8fbaca
Richard <82630326+richardsavala@users.noreply.github.com>
2021-07-05 23:58:46 -0600

    Add files via upload
    sidebar/modal - structure
    in Sidebar.js returned aside `sidebar show-sidebar`, div "sidebar-header", img {logo}
    added variables id, url, text, icon

---
+++ src/Sidebar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+
+const Sidebar = () => {
+  return (
+    <aside className={`sidebar show-sidebar`}>
+      <div className="sidebar-header">
+        <img src={logo} alt="coding addict" />
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((links) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}></a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
